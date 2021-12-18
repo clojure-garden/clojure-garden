bc245f5b8b785a4589e6ca7c9e53d289df64c23c
Ilshat Sultanov <just.sultanov@gmail.com>
2021-12-18 21:39:08 +0300

    Remove unnecessary scripts

--- scripts/setenv-digitalocean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pushd ./deployment/digitalocean || pushd ../deployment/digitalocean || exit
-
-grep -v '^#' .env
-export $(grep -v '^#' .env | xargs)
-
-popd || exit
--- scripts/setenv-local.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pushd ./deployment/local || pushd ../deployment/local || exit
-
-grep -v '^#' .env
-export $(grep -v '^#' .env | xargs)
-
-popd || exit
