6e1d7e32811db717089835be793631e05749ee07
Ilshat Sultanov <just.sultanov@gmail.com>
2021-12-28 23:08:56 +0500

    [frontend] Add landing page

---
+++ modules/frontend/src/platform/ui/pages/landing/view.cljs
@@ -0,0 +1,30 @@
+(ns platform.ui.pages.landing.view
+  (:require
+    [antd.core :as antd]
+    [platform.ui.components :as components]
+    [platform.ui.utils.string :refer [format]]))
+
+
+(defn content
+  []
+  [:div {:class "video"}
+   [antd/typography-title {:class "text"
+                           :strong true
+                           :type "success"}
+    "The navigator in the Clojure ecosystem"]
+   [:video {:autoPlay true
+            :muted    true
+            :loop     true}
+    [:source {:src  "ecosystem-fhd.mp4"
+              :type "video/mp4"}]]])
+
+
+(defn page
+  []
+  [antd/layout {:class "page landing-page"}
+   [antd/layout-header {:class "page-header"}
+    [components/header]]
+   [antd/layout-content {:class "page-content"}
+    [content]]
+   [antd/layout-footer {:class "page-footer"}
+    [components/footer]]])
