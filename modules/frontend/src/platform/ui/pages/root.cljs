19a3e7215b994d3b6a1d32447672ec51c94147e3
Ilshat Sultanov <just.sultanov@gmail.com>
2021-12-28 23:11:42 +0500

    [frontend] Add root page

---
+++ modules/frontend/src/platform/ui/pages/root.cljs
@@ -0,0 +1,29 @@
+(ns platform.ui.pages.root
+  (:require
+    [antd.core :as antd]
+    [antd.icons :as icons]
+    [clojure.string :as string]
+    [platform.ui.components :as components]
+    [platform.ui.pages.dashboard.view :as dashboard]
+    [platform.ui.pages.landing.view :as landing]
+    [platform.ui.pages.not-found.view :as not-found]
+    [re-frame.core :as rf]))
+
+
+(defn loading-page
+  []
+  [antd/layout {:class "loading-page"}
+   [antd/layout-content {:class "page-content"}
+    [components/spinner]]])
+
+
+(defn page
+  "Root page."
+  []
+  (if-not @(rf/subscribe [:app/initialized?])
+    [loading-page]
+    (let [route-name @(rf/subscribe [:navigation/route-name])]
+      (condp string/starts-with? (str route-name)
+        ":page/landing" [landing/page]
+        ":page/dashboard" [dashboard/page]
+        :else [not-found/page]))))
