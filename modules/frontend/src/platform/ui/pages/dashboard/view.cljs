6ea7f8b6f9af349e8d6459783eeea11d8cc4c86f
Ilshat Sultanov <just.sultanov@gmail.com>
2022-01-17 22:02:40 +0300

    [frontend] Add a simple github repositories components

--- modules/frontend/src/platform/ui/pages/dashboard/view.cljs
+++ modules/frontend/src/platform/ui/pages/dashboard/view.cljs
@@ -1,14 +1,47 @@
 (ns platform.ui.pages.dashboard.view
   (:require
     [antd.core :as antd]
-    [platform.ui.components :as components]))
+    [platform.ui.components :as components]
+    [platform.ui.utils.string :refer [format]]
+    [re-frame.core :as rf]))
+
+
+(defn repositories-list
+  [repositories]
+  (into [:div {:style {:margin-top "20px"}}]
+        (for [repos (partition-all 4 repositories)]
+          (into [antd/row {:gutter 2}]
+                (for [{:keys [name owner]} repos]
+                  (let [description (format "%s/%s" owner name)]
+                    [antd/col {:span 6}
+                     [antd/card
+                      [antd/card-meta
+                       {:description description}]]]))))))
+
+
+(defn select-tags
+  [tags selected-tags]
+  (into [antd/select {:mode        :multiple
+                      :placeholder "Select tags"
+                      :onChange    #(rf/dispatch [:github/select-tag %])
+                      :style       {:width "100%"}
+                      :value       selected-tags}]
+        (for [tag tags
+              :when (not (contains? selected-tags tag))]
+          [antd/select-option {:key tag} tag])))
 
 
 (defn content
   []
-  [antd/page-header
-   {:title "Dashboard page"
-    :onBack (constantly nil)}])
+  (let [tags          @(rf/subscribe [:github/repositories-tags])
+        selected-tags @(rf/subscribe [:github/selected-tags])
+        repositories  @(rf/subscribe [:github/repositories-by-tags])]
+    [antd/layout-content {:style {:margin-top "20px"}}
+     [antd/row
+      [antd/col {:span   20
+                 :offset 2}
+       [select-tags tags selected-tags]
+       [repositories-list repositories]]]]))
 
 
 (defn page
